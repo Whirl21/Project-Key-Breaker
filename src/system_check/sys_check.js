403b3ebd9770463a93a3afe4b74f3e75e9efff45
Skelly Mclane <74203498+SkellyM386@users.noreply.github.com>
2021-07-17 20:22:29 -0700

    Update sys_check.js

--- src/system_check/sys_check.js
+++ src/system_check/sys_check.js
@@ -1,74 +1,53 @@
-const fs = require('fs');
-
 exports = class Sys_Check {
-    constructor() {};
+    constructor() {
+        this.win32_validBrowsers = [
+          [/chrome/, "chrome path"],
+          [/firefox/, "firefox path"],
+          [/internet explorer/, "internetExplorer path"],
+          [/edge/, "edge path" ]
+          [/ie/, "ie path"]
+        ];
 
+        this.macOS_validBrowsers = [
+          [/chrome/, "chrome path"],
+          [/firefox/, "firefox path"],
+          [/safari/, "safari path"]
+        ]
+
+        this.linux_validBrowsers = [
+          [/chrome/, "chrome path"],
+          [/firefox/, "firefox path"]
+        ]
+    };
     /**
      * @param {String} browser
      * @returns 
      */
     static async win32(browser) {
         if (!browser) throw new Error("No browser specified");
-
-        switch (browser) {
-            case /chrome/gi.test(browser) == true:
-                if (fs.existsSync(/** path to chrome */) == true) return true;
-                else false;
-                break;
-            case (/firefox/gi).test(browser) == true:
-                if (fs.existsSync(/** path to firefox */) == true) return true;
-                else false;
-                break;
-            case (/edge/gi).test(browser) == true:
-                if (fs.existsSync(/** path to MS Edge */) == true) return true;
-                else false;
-                break;
-            case (/internet explorer/gi).test(browser) == true:
-                if (fs.existsSync(/** path to MS Internet Explorer */) == true) return true;
-                else false;
-                break;
-            case (/ie/gi).test(browser) == true:
-                if (fs.existsSync(/** path to MS Internet Explorer */) == true) return true;
-                else false;
-                break;
-        }
+        const foundBrowser = this.win32_validBrowsers.find(v => new RegExp(v[0], 'i').test(browser));
+        if(!foundBrowser)
+          throw Error("Invalid Browser Input");
+        if(!fs.existsSync(foundBrowser[1]))
+          throw Error("Browser Not Found >:3");
+    };
     };
 
     static async macOS(browser) {
         if (!browser) throw new Error("No browser specified");
-
-        switch (browser) {
-            case /chrome/gi.test(browser) == true:
-                if (fs.existsSync(/** path to chrome */) == true) return true;
-                else false;
-                break;
-            case (/firefox/gi).test(browser) == true:
-                if (fs.existsSync(/** path to firefox */) == true) return true;
-                else false;
-                break;
-            case (/safari/gi).test(browser) == true:
-                if (fs.existsSync(/** path to safari */) == true) return true;
-                else false;
-                break;
-        }
+        const foundBrowser = this.macOS_validBrowsers.find(v => new RegExp(v[0], 'i').test(browser));
+        if(!foundBrowser)
+          throw Error("Invalid Browser Input");
+        if(!fs.existsSync(foundBrowser[1]))
+          throw Error("Browser Not Found >:3");
     }
 
     static async linux(browser) {
         if (!browser) throw new Error("No browser specified");
-
-        switch (browser) {
-            case /chrome/gi.test(browser) == true:
-                if (fs.existsSync(/** path to chrome */) == true) return true;
-                else false;
-                break;
-            case (/firefox/gi).test(browser) == true:
-                if (fs.existsSync(/** path to firefox */) == true) return true;
-                else false;
-                break;
-            case (/etc/gi).test(browser) == true:
-                if (fs.existsSync(/** path to other browsers */) == true) return true;
-                else false;
-                break;
-        }
+        const foundBrowser = this.linux_validBrowsers.find(v => new RegExp(v[0], 'i').test(browser));
+        if(!foundBrowser)
+          throw Error("Invalid Browser Input");
+        if(!fs.existsSync(foundBrowser[1]))
+          throw Error("Browser Not Found >:3");
     }
 }
